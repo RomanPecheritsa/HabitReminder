6e4b3b442fb46fbdfa676c17cde57693ebd7f369
pecheritsa <pecheritsa.roman@gmail.com>
2024-12-01 19:17:30 +0300

    add serializer HabitSerializer

--- habits/serializers.py
+++ habits/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from habits.models import Habit
 
 
-class HabitSerializers(serializers.ModelSerializer):
+class HabitSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Habit
