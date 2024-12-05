666840fc8599e545d56ba9c4061cc3877ff2e91d
pecheritsa <pecheritsa.roman@gmail.com>
2024-12-05 19:55:18 +0300

    add path auth, schema, swagger, redoc

--- habit_reminder/urls.py
+++ habit_reminder/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("habits/", include("habits.urls", namespace="habits")),
+    path("auth/", include("users.urls", namespace="users")),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
