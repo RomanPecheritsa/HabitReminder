3cc0c0bce347407d90a8b98fa3562ebc3ef7c8a1
pecheritsa <pecheritsa.roman@gmail.com>
2024-12-05 21:57:17 +0300

    add field tg_chat_id

--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    tg_chat_id = models.CharField(
+        max_length=50, verbose_name="Telegram ID", blank=True, null=True
+    )
