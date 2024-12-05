5a679b59d4113885436783a9dded7966571b619e
pecheritsa <pecheritsa.roman@gmail.com>
2024-12-05 21:58:38 +0300

    linter module

--- habits/serializers.py
+++ habits/serializers.py
@@ -8,4 +8,4 @@ class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = "__all__"
-        read_only_fields = ('user',)
+        read_only_fields = ("user",)
