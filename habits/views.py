cfb9d2d846a9e80e3fdb89178d23815a79c2a57c
pecheritsa <pecheritsa.roman@gmail.com>
2024-12-05 19:58:32 +0300

    add HabitViewSet.get_queryset, perform_create, add PublicListAPIView

--- habits/views.py
+++ habits/views.py
@@ -1,11 +1,22 @@
-from rest_framework import viewsets
-from rest_framework.pagination import PageNumberPagination
+from rest_framework import viewsets, generics
 
 from habits.models import Habit
+from habits.permissions import IsOwner
 from habits.serializers import HabitSerializer
 
 
 class HabitViewSet(viewsets.ModelViewSet):
-    queryset = Habit.objects.all()
     serializer_class = HabitSerializer
-    pagination_class = PageNumberPagination
+    permission_classes = (IsOwner,)
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PublicListAPIView(generics.ListAPIView):
+    queryset = Habit.public_habits.all()
+    serializer_class = HabitSerializer
+
