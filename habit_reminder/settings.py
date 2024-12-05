3ac730ace01252a3e6d8053398811f4f814bf095
pecheritsa <pecheritsa.roman@gmail.com>
2024-12-05 19:54:34 +0300

    install app drf_spectacular, add DEFAULT_SCHEMA_CLASS drf , add SPECTACULAR_SETTINGS

--- habit_reminder/settings.py
+++ habit_reminder/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "drf_spectacular",
     "users",
     "habits",
 ]
@@ -91,6 +92,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Habit Reminder API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
 }
 
 SIMPLE_JWT = {
